02cd547048132b6edacd9f0840c09c420104ee15
Your Name <you@example.com>
2025-02-28 05:55:13 +0300

    new for render5

--- resource_center/settings.py
+++ resource_center/settings.py
@@ -13,6 +13,11 @@ import os
 import dj_database_url
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
